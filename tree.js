5f21f9d01268149511f7d2b33ebe7655e272a240
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-03-01 13:05:41 -0800

    completed Tree exercises

--- tree.js
+++ tree.js
@@ -15,20 +15,73 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    // If there is not element in the tree
+    if (!this.root) return 0;
+
+    // Storing sum to total
+    let total = this.root.val;
+
+    function sumCal(node) {
+      // Iterate over each child node
+      for (let child of node.children) {
+        // Add value of child node to total
+        total += child.val;
+
+        // Recurse if child node has another child node below it
+        if (child.children.length > 0) sumCal(child);
+      }
+    }
+
+    sumCal(this.root);
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    // If there is not element in the tree
+    if (!this.root) return 0;
 
+    let count = this.root.val % 2 === 0 ? 1 : 0;
+
+    function countChild(node) {
+      // Iterate over each child node
+      for (let child of node.children) {
+        // Add value of child node to total
+        if (child.val % 2 === 0) count++;
+
+        // recurse if child node has another child node below it
+        if (child.children.length > 0) countChild(child);
+      }
+    }
+
+    countChild(this.root);
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    // If there is not element in the tree
+    if (!this.root) return 0;
 
+    // store count
+    let count = this.root.val > lowerBound ? 1 : 0;
+
+    function countChild(node) {
+      // Iterate over each child node
+      for (let child of node.children) {
+        // Add value of child node to total
+        if (child.val > lowerBound) count++;
+
+        // recurse if child node has another child node below it
+        if (child.children.length > 0) countChild(child);
+      }
+    }
+
+    countChild(this.root);
+    return count;
   }
 }
 
